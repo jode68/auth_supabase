738c2c55f869752f4127d6eec6c031872573d53c
JoDe68 <xumepao@yahoo.it>
2025-01-18 23:46:19 +0100

    1

--- lib/screen/base/base_page.dart
+++ lib/screen/base/base_page.dart
@@ -7,18 +7,33 @@ class BasePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.green.shade200,
       appBar: AppBar(
         title: const Text('Auth Supabase'),
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.green.shade400,
+        foregroundColor: Colors.white,
         centerTitle: true,
       ),
       body: ListView(
         padding: const EdgeInsets.all(32),
         children: [
           SizedBox(height: 32),
-          SizedBox(
-            height: 200,
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green.shade600,
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 8,
+                ),
+              ],
+            ),
+            height: 250,
             child: Image.asset(
               'assets/images/supabase.png',
               fit: BoxFit.contain,
